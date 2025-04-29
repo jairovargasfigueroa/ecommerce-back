2eb79132df0f21c9c89713b0052932c7af8f41df
jvargas <jairo.270203@gmail.com>
2025-04-29 03:24:24 -0400

    fix: servir index.html desde staticfiles en vez de usar TemplateView

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path,re_path
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from waitress import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),   
@@ -31,7 +32,11 @@ urlpatterns = [
     path('api/notificaciones/', include('notificaciones.urls')),
 
     # path('api/recomendaciones/', include('recomendaciones.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^.*$', serve, {
+    'path': 'index.html',
+    'document_root': settings.STATIC_ROOT
+}),
+
 
 ]
 
